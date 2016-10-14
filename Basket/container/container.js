d3288c63ea89b4a1c21da7a5828b88a732b5eeac
bowka <bowka@users.noreply.github.com>
2016-10-14 10:54:13 +0200

    Update container.js

--- Basket/container/container.js
+++ Basket/container/container.js
@@ -1,14 +1,9 @@
 import { connect } from 'react-redux';
-import * as ActionTypes from '../constants/ActionTypes';
-import * as selector from '../selectors/selectors'
+import * as selector from '../selectors/selector'
 import App from '../components/App'
 
-const mapStateToProps = appState => ({
-	items: selector.getItems(appstate)
-})
-const mapDispatchToProps = dispatch =>({
-	onIncrement: (item) => dispatch({type: ActionTypes.INC data:item})
-	onDecrement: (item) => dispatch({type: ActionTypes.DEC data:item})
-});
-export default connect (mapStateToProps,mapDispatchToProps)(App); 
 
+const mapStateToProps = appState => ({
+	items: selector.getItems(appState)
+});
+export default connect(mapStateToProps)(App); 
